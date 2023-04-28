b00c2b715843c8c6a1bb7b049ede0822611cf180
jjavier <razornice@gmail.com>
2023-04-28 16:35:49 -0400

    user-side assoc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :hostings, inverse_of :host, class_name: "Event"
+
+  has_many :invitations
+  has_many :appearances, through: :invitations, source: :event
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
