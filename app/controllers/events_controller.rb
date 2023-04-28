e71d9d86c11e450e3ef2db56fdda50b214c056b7
jjavier <razornice@gmail.com>
2023-04-28 17:30:47 -0400

    events new and create controlers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,40 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+
+  end
+
+  def new
+    @event = current_user.hostings.build
+  end
+
+  def create
+    @event = current_user.hostings.build(event_params)
+    if @event.save 
+      redirect_to @event_url(@event)
+      notice: "Event succesfully created. Have fun!"
+    else
+      flash.now[:error] = "It was not possible to create the event" 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:day, :place)
 end
