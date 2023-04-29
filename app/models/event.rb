277ae0405b2febc66023f18d6298ff212f52b867
jjavier <razornice@gmail.com>
2023-04-28 23:15:17 -0400

    events assoc.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: "User", foreign_key: "host_id"
+
+  has_many :invitations
+  has_many :guests, through: :invitations, source: :user
 end
